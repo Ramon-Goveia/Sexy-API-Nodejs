7546c6c744e210d8feae85b2903cb5c849ea0310
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-11-19 12:56:00 -0300

    Criando as functions coms os metodos http e finalizando API

--- CreateProduct/index.js
+++ CreateProduct/index.js
@@ -1,15 +1,17 @@
-const { MongoClient } = require('mongodb');
 const createMongoClient = require('../shared/mongoClient');
 
+
 module.exports = async function (context, req) {
+    const product = req.body;
+
     const { client: MongoClient, closeConnectionFn } = await createMongoClient();
     const Products = MongoClient.collection('products');
-    const res = await Products.find({});
-    const body = await res.toArray();
+
+    const res = await Products.insert(product);
 
     closeConnectionFn();
     context.res = {
-        status: 200,
-        body,
+        status: 201,
+        body: res,
     };
 };
--- UpdateProduct/index.js
+++ UpdateProduct/index.js
@@ -1,15 +1,22 @@
-const { MongoClient } = require('mongodb');
+const { ObjectID } = require('mongodb');
 const createMongoClient = require('../shared/mongoClient');
 
+
 module.exports = async function (context, req) {
+    const { id } = req.params;
+    const product = req.body;
+
     const { client: MongoClient, closeConnectionFn } = await createMongoClient();
     const Products = MongoClient.collection('products');
-    const res = await Products.find({});
-    const body = await res.toArray();
+
+    const res = await Products.findOneAndUpdate(
+        {_id: ObjectID(id)},
+        {$set: product},
+    );
 
     closeConnectionFn();
     context.res = {
         status: 200,
-        body,
+        body: res,
     };
 };
